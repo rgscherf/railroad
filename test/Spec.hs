3a6f29ff6076795f85421b29753940ab1fd59891
rgscherf <rgscherf@gmail.com>
2016-11-24 20:40:52 -0500

    basic unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Text  as T
+import           Railway
+import           Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Backward application" $ do
+        it "does simple application" $ do
+            ( (+1) <| 3 )
+                `shouldBe` 4
+        it "does compound simple application" $ do
+            ( (+1) <| (*2) <| 3 )
+                `shouldBe` 7
+    describe "Backward application" $ do
+        it "does simple application" $ do
+            ( 3 |> (+1) )
+                `shouldBe` 4
+        it "does compound simple application" $ do
+            ( 3 |> (*2) |> (+1) )
+                `shouldBe` 7
+        it "does a compound string operation" $ do
+            ( "hello"
+                |> T.toUpper
+                |> T.tail
+                |> T.reverse
+                |> T.unpack )
+                    `shouldBe` "OLLE"
